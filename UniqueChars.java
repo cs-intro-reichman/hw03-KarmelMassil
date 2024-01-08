45c2ad70ed35fe1b0dd5c16709fb1d568e86b7ad
KarmelMassil <154368367+KarmelMassil@users.noreply.github.com>
2024-01-09 01:27:35 +0200

    draft_1 UniqueChars.java

--- UniqueChars.java
+++ UniqueChars.java
@@ -2,7 +2,7 @@
 public class UniqueChars {
     public static void main(String[] args) {  
         String str = args[0];
-        System.out.println(uniqueChars(str));
+		        System.out.println(uniqueChars(str));
     }
 
     /**
@@ -10,8 +10,20 @@ public class UniqueChars {
      * except that all the duplicate characters are removed,
      * unless they are space characters.
      */
-    public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
-    }
+    public static String uniqueChars(String str) {
+        String s = "";
+		int l = str.length();
+	    for(int i = 0; i < l; i++) {
+		    char d = str.charAt(i);
+			if(d != ' ') {
+			if(str.indexOf(d) == i){
+				s += d;
+			}
+			}
+			else {
+				s += ' ';
+			}
+		}
+		return s;
+	}
 }
