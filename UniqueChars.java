a5bcaa32628cafb6e31e2ef4d6b1038e406780d2
KarmelMassil <karmelreuben.massil@post.runi.ac.il>
2024-01-09 12:59:15 +0200

    Trial

--- UniqueChars.java
+++ UniqueChars.java
@@ -1,15 +1,10 @@
 /** String processing exercise 2. */
-public class UniqueChars {
+public class uniquechars {
     public static void main(String[] args) {  
         String str = args[0];
 		        System.out.println(uniqueChars(str));
     }
 
-    /**
-     * Returns a string which is identical to the original string, 
-     * except that all the duplicate characters are removed,
-     * unless they are space characters.
-     */
     public static String uniqueChars(String str) {
         String s = "";
 		int l = str.length();
