f4ebcc942ed151af99e1c7df689d42943cda8323
KarmelMassil <karmelreuben.massil@post.runi.ac.il>
2024-01-11 19:38:46 +0200

    fun

--- LowerCase.java
+++ LowerCase.java
@@ -1,15 +1,15 @@
 public class LowerCase {
     public static void main (String[] args) {
         String str = args[0];
-        System.out.println (lowerCase(str));
+        System.out.println (lowerCase (str));
     }
 
     public static String lowerCase (String s) {
         String w = "";
         int l = s.length();
-        for (int i = 0; i<l; i++) {
+        for (int i = 0; i < l; i++) {
             char temp = s.charAt(i);
-            if (temp >='A' && temp <= 'z') {
+            if (temp >='A' && temp <= 'Z') {
                 temp += 32;
                 w += temp;
             } else w += temp;
