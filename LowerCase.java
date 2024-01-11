42fcc224e76dd5ef0d10839c381670228283ae34
KarmelMassil <karmelreuben.massil@post.runi.ac.il>
2024-01-11 19:09:52 +0200

    final

--- LowerCase.java
+++ LowerCase.java
@@ -1,19 +1,19 @@
 public class LowerCase {
-    public static void main(String[] args) {  
+    public static void main (String[] args) {
         String str = args[0];
-		System.out.println(lowerCase(str));
+        System.out.println (lowerCase(str));
     }
 
-    public static String lowerCase(String s) {
-		String w = "";
-	    int l = s.length();
-	for(int i = 0; i < l; i++){
-		char temp = s.charAt(i);
-		if(temp >= 'A' && temp <= 'Z'){
-			temp += 32;
-			w += temp;
-		} else w += temp;
-	    }
-		return w;
+    public static String lowerCase (String s) {
+        String w = "";
+        int l = s.length();
+        for (int i = 0; i<l; i++) {
+            char temp = s.charAt(i);
+            if (temp >='A' && temp <= 'z') {
+                temp += 32;
+                w += temp;
+            } else w += temp;
+        }
+        return w;
     }
 }
--- UniqueChars.java
+++ UniqueChars.java
@@ -1,23 +1,19 @@
 public class UniqueChars {
-    public static void main(String[] args) {  
+    public static void main (String[] args) {
         String str = args[0];
-		        System.out.println(uniqueChars(str));
+        System.out.println (uniqueChars (str));
     }
 
-    public static String uniqueChars(String str) {
+    public static String uniqueChars (String str) {
         String s = "";
-		int l = str.length();
-	    for(int i = 0; i < l; i++) {
-		    char temp = str.charAt(i);
-			if(temp != ' ') {
-			if(str.indexOf(temp) == i){
-				s += temp;
-			}
-			}
-			else {
-				s += ' ';
-			}
-		}
-		return s;
-	}
+        int l = str.length();
+        for (int i = 0; i < l; i++) {
+            char temp = str.charAt(i);
+            if (temp != ' ') {
+                if (str.indexOf (temp) == i ) {
+                    s += temp;
+                }
+            } else s+= ' ';
+        } return s;
+    }
 }
